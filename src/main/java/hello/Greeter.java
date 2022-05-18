a96528ee916b7dd1fda1414f15b1fac9243ec0ee
bokumoku <jati.hiliamsyah@gmail.com>
2022-05-18 21:42:39 +0900

    clean

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -4,8 +4,4 @@ public class Greeter {
     public String sayHello() {
         return "DevOps Project for AchiStar Technologies";
     }
-
-    private String sayHelloAgain(){
-        return "DevOps Project for MKP Technologies";
-    }
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -9,6 +9,5 @@ public class HelloWorld {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-        System.out.println(greeter.sayHelloAgain());
     }
 }
