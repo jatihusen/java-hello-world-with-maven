1a32b87da8bd4a2185aae5ecc685c0fade16a0e1
bokumoku <jati.hiliamsyah@gmail.com>
2022-05-18 21:07:51 +0900

    Update Greeter

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -4,4 +4,8 @@ public class Greeter {
     public String sayHello() {
         return "DevOps Project for AchiStar Technologies";
     }
+
+    private String sayHelloAgain(){
+        return "DevOps Project for MKP Technologies";
+    }
 }
